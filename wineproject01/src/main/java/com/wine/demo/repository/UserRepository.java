fddf75c5fa53b16232b847957a81ac03dc732413
CHO GYEONGJIN <kyjj1024@gmail.com>
2023-11-13 16:01:39 +0900

    fix final
    fix location

--- wineproject01/src/main/java/com/wine/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wine.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wine.demo.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
